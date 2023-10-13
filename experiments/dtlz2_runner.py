1a8aaf14f0e7948b44b2b7f06431ec1e8e978d6c
RaulAstudillo06 <astudillo0606@gmail.com>
2023-10-13 00:02:57 -0700

    Try DTLZ2

---
+++ experiments/dtlz2_runner.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import torch
+
+from botorch.settings import debug
+from botorch.test_functions.multi_objective import DTLZ2
+
+torch.set_default_dtype(torch.float64)
+torch.autograd.set_detect_anomaly(False)
+debug._set_state(False)
+
+script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+print(script_dir[:-12])
+sys.path.append(script_dir[:-12])
+
+from src.experiment_manager import experiment_manager
+from src.get_noise_level import get_noise_level
+
+
+# Attribute function
+input_dim = 3
+num_attributes = 2
+attribute_func = DTLZ2(dim=input_dim, negate=True)
+
+# Algos
+algo = "SDTS"
+# algo = "SDTS-HS"
+# algo = "I-PBO-DTS"
+# algo = "Random"
+
+# Estimate noise level
+comp_noise_type = "logit"
+if False:
+    noise_level = get_noise_level(
+        attribute_func,
+        input_dim,
+        target_error=0.2,
+        top_proportion=0.01,
+        num_samples=10000000,
+        comp_noise_type=comp_noise_type,
+    )
+    print(noise_level)
+    print(e)
+
+noise_level = [0.01, 0.01]
+
+# Run experiment
+if len(sys.argv) == 3:
+    first_trial = int(sys.argv[1])
+    last_trial = int(sys.argv[2])
+elif len(sys.argv) == 2:
+    first_trial = int(sys.argv[1])
+    last_trial = int(sys.argv[1])
+
+experiment_manager(
+    problem="dtlz2",
+    attribute_func=attribute_func,
+    input_dim=input_dim,
+    num_attributes=num_attributes,
+    comp_noise_type=comp_noise_type,
+    comp_noise=noise_level,
+    algo=algo,
+    batch_size=2,
+    num_init_queries=2 * (input_dim + 1),
+    num_algo_iter=2,
+    first_trial=first_trial,
+    last_trial=last_trial,
+    restart=True,
+)
