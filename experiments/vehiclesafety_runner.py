981fbfa61f46dbfd2b4f3862f4c704a7e9022fe4
RaulAstudillo06 <astudillo0606@gmail.com>
2023-10-22 22:06:49 -0700

    Update Vehicle Safety runner

--- experiments/vehiclesafety_runner.py
+++ experiments/vehiclesafety_runner.py
@@ -8,7 +8,7 @@ from botorch.test_functions.multi_objective import VehicleSafety
 from torch import Tensor
 
 torch.set_default_dtype(torch.float64)
-torch.autograd.set_detect_anomaly(True)
+torch.autograd.set_detect_anomaly(False)
 debug._set_state(False)
 
 script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -16,12 +16,10 @@ print(script_dir[:-12])
 sys.path.append(script_dir[:-12])
 
 from src.experiment_manager import experiment_manager
-
-# from kumaraswamy_utility import KumaraswamyCDFProduct
-from piecewiselinear_utility import PiecewiseLinear
+from src.get_noise_level import get_noise_level
 
 
-# Objective function
+# Attribute function
 vehiclesafety_func = VehicleSafety(negate=True)
 input_dim = vehiclesafety_func.dim
 num_attributes = vehiclesafety_func.num_objectives
@@ -33,31 +31,28 @@ def attribute_func(X: Tensor) -> Tensor:
     return output
 
 
-# normalized_attribute_bounds = torch.tensor(
-# [
-# [0, 0, 0],
-# [1, 1, 1],
-# ]
-# )
-# concentration1 = torch.tensor([0.5, 1, 1.5])
-# concentration2 = torch.tensor([1.0, 2.0, 3.0])
-
-# utility_func = KumaraswamyCDFProduct(
-# concentration1=concentration1, concentration2=concentration2, Y_bounds=normalized_attribute_bounds
-# )
-
-beta1 = torch.tensor([2, 6, 8])
-beta2 = torch.tensor([1, 2, 2])
-thresholds = torch.tensor([0.5, 0.8, 0.8])
-utility_func = PiecewiseLinear(beta1=beta1, beta2=beta2, thresholds=thresholds)
-
 # Algos
-algo = "ScalarizedTS"
-model_type = "Multioutput"
+algo = "SDTS"
+# algo = "SDTS-HS"
+# algo = "I-PBO-DTS"
+# algo = "Random"
 
-# estimate noise level
+# Estimate noise level
 comp_noise_type = "logit"
-noise_level = 0.0001
+if False:
+    noise_level = get_noise_level(
+        attribute_func,
+        input_dim,
+        target_error=0.2,
+        top_proportion=0.01,
+        num_samples=10000000,
+        comp_noise_type=comp_noise_type,
+    )
+    print(noise_level)
+    print(e)
+
+
+noise_level = [0.6146, 0.0989, 0.0021]
 
 # Run experiment
 if len(sys.argv) == 3:
@@ -70,13 +65,11 @@ elif len(sys.argv) == 2:
 experiment_manager(
     problem="vehiclesafety",
     attribute_func=attribute_func,
-    utility_func=utility_func,
     input_dim=input_dim,
     num_attributes=num_attributes,
     comp_noise_type=comp_noise_type,
     comp_noise=noise_level,
     algo=algo,
-    model_type=model_type,
     batch_size=2,
     num_init_queries=2 * (input_dim + 1),
     num_algo_iter=100,
