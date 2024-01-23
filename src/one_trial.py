8262b840e7075bd340d8159f8e6ea78234a962fd
RaulAstudillo06 <astudillo0606@gmail.com>
2024-01-22 23:50:01 -0800

    Implement qParEGO

--- src/one_trial.py
+++ src/one_trial.py
@@ -10,11 +10,17 @@ import torch
 from botorch.acquisition.multi_objective.monte_carlo import (
     qExpectedHypervolumeImprovement,
 )
+from botorch.acquisition.monte_carlo import (
+    qExpectedImprovement
+)
+from botorch.acquisition.objective import GenericMCObjective
 from botorch.models.model import Model
 from botorch.sampling.normal import SobolQMCNormalSampler
 from botorch.utils.multi_objective.box_decompositions.non_dominated import (
     FastNondominatedPartitioning,
 )
+from botorch.utils.multi_objective.scalarization import get_chebyshev_scalarization
+from botorch.utils.sampling import sample_simplex
 from torch import Tensor
 
 from src.acquisition_functions.dueling_thompson_sampling import (
@@ -299,6 +305,24 @@ def get_new_suggested_query(
             raw_samples,
             scalarize=False,
         )
+    elif algo == "qParEGO":
+        mean_at_train_inputs = model.posterior(model.train_inputs[0][0]).mean.detach()
+        weights = sample_simplex(mean_at_train_inputs.shape[-1]).squeeze()
+        chebyshev_scalarization = GenericMCObjective(get_chebyshev_scalarization(weights=weights, Y=mean_at_train_inputs))
+        acquisition_function = qExpectedImprovement(
+            model=model,
+            objective=chebyshev_scalarization,
+            best_f=chebyshev_scalarization(mean_at_train_inputs).max(),
+            sampler=SobolQMCNormalSampler(sample_shape=torch.Size([128])),
+        )
+        new_query = optimize_acqf_and_get_suggested_query(
+            acq_func=acquisition_function,
+            bounds=standard_bounds,
+            batch_size=batch_size,
+            num_restarts=num_restarts,
+            raw_samples=raw_samples,
+        )
+        new_query = new_query.unsqueeze(0)
     elif algo == "qEHVI":
         mean_at_train_inputs = model.posterior(model.train_inputs[0][0]).mean.detach()
         ref_point = mean_at_train_inputs.min(0).values
